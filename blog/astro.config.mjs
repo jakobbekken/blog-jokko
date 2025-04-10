7eae3a193e7faa3d022a4ac501f565b36136a17b
Jakob Moen Bekken <jakobmb@uia.no>
2025-04-10 22:50:33 +0000

    Added KaTeX support in MDX

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 import fs from "fs";
 
 const rosePineCustom = JSON.parse(
@@ -15,6 +18,8 @@ export default defineConfig({
 	integrations: [mdx(), sitemap(), tailwind()],
 	server: { port: 3000, host: "0.0.0.0" },
 	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
 		shikiConfig: {
 			theme: rosePineCustom,
 		},
